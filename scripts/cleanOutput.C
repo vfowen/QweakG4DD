7e2d4e5b3693af128b0000fe567657a6a63f5a75
Ciprian Gal <ciprian@jlab.org>
2015-04-28 13:13:45 -0400

    remove bugs and add some comments

--- scripts/cleanOutput.C
+++ scripts/cleanOutput.C
@@ -1,6 +1,10 @@
 void cleanOutput(){
-  string patern="";
   
+  string patern="V_1160_010.00*";
+  cout<<"removeSegmentation"<<endl;
+  removeSegmentation(patern);
+  cout<<"Log files don't exit"<<endl;
+  removeNoLog(patern);
 }
 
 void removeSegmentation(string pat){
@@ -8,30 +12,35 @@ void removeSegmentation(string pat){
 
   ifstream fin("z_Seg.lst");
   string data;
-  while(fin>>data){
+  while(fin>>data){//==>
+    fin>>data;//get folder
     TString file(data.c_str());
-    file.Remove(0,file.First('_')-2);
     file.Remove(file.First('/'),file.Length());
     gSystem->Exec(Form("rm -rf %s",file.Data()));
+    cout<<data<<" removing folder "<<file.Data()<<endl;
+    fin>>data;// <==
   }
-  
+  fin.close();
   gSystem->Exec("rm -rf z_Seg.lst");
 }
 
 
 void removeNoLog(string pat){
-  gSystem->Exec(Form("ls %s/log/log.out > z_log.lst",pat.c_str()));
+  gSystem->Exec(Form("ls -d %s > z_log.lst",pat.c_str()));
 
   ifstream fin("z_log.lst");
   string data;
   while(fin>>data){
-    ifstream tst(data);
-    if(!tst){    
+    data+="/log/log.out";
+    ifstream tst(data.c_str(),ios::in);
+    if(!tst.good()){    
       TString file(data.c_str());
-      file.Remove(0,file.First('_')-2);
       file.Remove(file.First('/'),file.Length());
       gSystem->Exec(Form("rm -rf %s",file.Data()));
+      cout<<data<<" removing folder "<<file.Data()<<endl;
     }
-  }  
+    else tst.close();
+  }
+  fin.close();
   gSystem->Exec("rm -rf z_log.lst");
 }
