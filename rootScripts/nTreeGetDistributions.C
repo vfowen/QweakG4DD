9540f7516d3ef19354de686e4209c6a0b822ef19
Ciprian Gal <ciprian@jlab.org>
2015-05-26 14:36:33 -0400

    update with mean bean position calc

--- rootScripts/nTreeGetDistributions.C
+++ rootScripts/nTreeGetDistributions.C
@@ -19,10 +19,14 @@ void nTreeFirstIntOneSet(string flist){
   string gnm[5]={"Primary e-","Non primary e-","Non primary e+","All e#pm","Photons"};
   
   TFile *fout=new TFile("o_distributions.root","UPDATE");
-  TH1F *distr[10];
+  TH1F *distr[20];
   for(int i=0;i<5;i++){
     distr[i]  =new TH1F(Form("hX_%s_%d",onm.c_str(),i)   ,Form("%s ; Xpos[cm]",gnm[i].c_str())  ,200,-100,100);
     distr[i+5]=new TH1F(Form("hangX_%s_%d",onm.c_str(),i),Form("%s ; Ang X[deg]",gnm[i].c_str()),180,-90,90);
+  }
+  for(int i=10;i<15;i++){
+    distr[i]  =new TH1F(Form("mX_%s_%d",onm.c_str(),i)   ,Form("Mean for %s ; Xpos[cm]",gnm[i].c_str())  ,200,-100,100);
+    distr[i+5]=new TH1F(Form("mangX_%s_%d",onm.c_str(),i),Form("Mean for %s ; Ang X[deg]",gnm[i].c_str()),180,-90,90);
   }
 
   while(fin>>data){
@@ -30,10 +34,10 @@ void nTreeFirstIntOneSet(string flist){
     fl.Remove(fl.Last('/')+1,fl.Length());
     fl+="o_anaTree.root";
     cout<<fl.Data()<<endl;
-    doAna(fl.Data(),distr);    
+    doAnaTree(fl.Data(),distr);    
   }
 
-  for(int i=0;i<10;i++){
+  for(int i=0;i<20;i++){
     fout->cd();
     distr[i]->Write();
   }
@@ -43,7 +47,7 @@ void nTreeFirstIntOneSet(string flist){
 }
 
 
-double doAna(char *fn, TH1F *hA[10])
+void doAna(char *fn, TH1F *hA[10])
 {
   TString nb(fn);
   nb.Remove(nb.Last('/'),nb.Length());
@@ -74,5 +78,92 @@ double doAna(char *fn, TH1F *hA[10])
     hA[i]->Add(ha[i]);
 
   fin->Close();
+}
+ 
+void doAnaTree(char *fn, TH1F *hA[20])
+{
+  TString nb(fn);
+  nb.Remove(nb.Last('/'),nb.Length());
+  nb.Remove(0,nb.Last('_')+1);
+  int n=nb.Atoi();
+  
+  TFile *fin=TFile::Open(fn,"READ");
+  TTree *t=(TTree*)fin->Get("th");
+
+  float x,e;
+  int hasPar,nInt, pType;
+  float poly,polz,angX;
+
+  t->SetBranchAddress("x",&x);
+  t->SetBranchAddress("angX",&angX);
+  t->SetBranchAddress("E",&e);
+  t->SetBranchAddress("nInt",&nInt);
+  t->SetBranchAddress("hasParent",&hasPar);
+  t->SetBranchAddress("pType",&pType);
+  t->SetBranchAddress("poly",&poly);
+  t->SetBranchAddress("polz",&polz);
+
+  int nv=t->GetEntries();
+
+  for(int i=0;i<nv;i++){
+    t->GetEvent(i);
+    if(hasPar!=0) continue;
+    if(nInt!=1) continue;
+    if(e<=1 ) continue;
+    if( fabs(angX)>90 ) continue;
+
+    if(fabs(pType)==11){
+      ha[3]->Fill(x);
+      ha[8]->Fill(angX);
+      updateMean(x,angX,hA[13],hA[18]);
+    }
+    
+    if(pType==22){
+      hA[4]->Fill(x);
+      hA[9]->Fill(angX);
+      updateMean(x,angX,hA[14],hA[19]);
+    }else if(pType==11){
+      if(sqrt(pow(poly,2)+pow(polz,2))>0.1){
+	ha[0]->Fill(x);
+	ha[5]->Fill(angX);
+	updateMean(x,angX,hA[10],hA[15]);	
+      }else{
+	ha[1]->Fill(x);
+	ha[6]->Fill(angX);
+	updateMean(x,angX,hA[11],hA[16]);
+      }
+    }else if(pType==-11){
+      ha[2]->Fill(x);
+      ha[7]->Fill(angX);
+      updateMean(x,angX,hA[12],hA[17]);
+    }
+  }
+  
+  fin->Close();
+}
+
+void updateMean(float x, float ax, TH1F *a, TH1F *b){
+  int bx=a->GetXaxis()->FindBin(x);
+  int bax=b->GetXaxis()->FindBin(ax);
+
+  if(a->GetBinError(bx)==0){
+    a->SetBinContent(bx,x);
+    a->SetBinError(bx,1);
+  }else{
+    float dval=a->GetBinError(bx)+1;
+    float val=(x+(dval-1)*a->GetBinContent(bx))/dval;
+    a->SetBinContent(bx,val);
+    a->SetBinError(bx,dval);    
+  }
+
+  if(b->GetBinError(bax)==0){
+    b->SetBinContent(bax,x);
+    b->SetBinError(bax,1);
+  }else{
+    float dval=b->GetBinError(bax)+1;
+    float val=(ax+(dval-1)*b->GetBinContent(bax))/dval;
+    b->SetBinContent(bax,val);
+    b->SetBinError(bax,dval);    
+  }
 }
  
