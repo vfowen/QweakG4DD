ac78f9e54b4b6483e26ae157dfe838e4260e95b6
Ciprian Gal <ciprian@jlab.org>
2015-06-23 11:02:09 -0400

    remove warning for myEventCounter. reformat

--- src/QweakSimPrimaryGeneratorAction.cc
+++ src/QweakSimPrimaryGeneratorAction.cc
@@ -51,13 +51,7 @@ QweakSimPrimaryGeneratorAction::QweakSimPrimaryGeneratorAction( QweakSimUserInfo
     
     // get my messenger
     myMessenger = new QweakSimPrimaryGeneratorActionMessenger(this);
-    
-    // get my messenger
-    myMessenger = new QweakSimPrimaryGeneratorActionMessenger(this);
-    
-    // initialize my own event counter
-    // myEventCounter = 0;
-    
+        
     G4int n_particle = 1;
     particleGun = new G4ParticleGun(n_particle);
     SetParticleType("e-");
@@ -81,98 +75,78 @@ QweakSimPrimaryGeneratorAction::~QweakSimPrimaryGeneratorAction()
 
 void QweakSimPrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
 {
-    /** \page target_energy_loss target energy loss
-     * Pre-scattering (external) target energy loss is simulated by throwing one primary particle
-     * starting at 30 cm upstream of the target and letting it propagate to the actual z vertex position
-     * in the target.  A second primary particle is then thrown with the angle and transverse position
-     * of the first primary at that point.  Event time starts when the first primary is thrown.
-     * \todo need to double-check that position and momentum are properly transferred to second primary
-     */
-
-    G4int myEventCounter = myUserInfo->GetPrimaryEventNumber();
-
-    if (myEventCounter%1000==0) G4cout << "*=== Event number = " << myEventCounter << " ===*" << G4endl;
-
-    G4double myPositionX, myPositionY, myPositionZ, myVertexZ;
-    G4double myNormMomentumX, myNormMomentumY, myNormMomentumZ;
-    G4double E_beam;  // Energy of the incoming and outgoing particle
-
-
-    myPositionX =  myUserInfo->GetBeamPositionX();
-    myPositionY =  myUserInfo->GetBeamPositionY();
-    myPositionZ =  myUserInfo->GetBeamPositionZ();
-
-    myNormMomentumX  = tan(myUserInfo->GetNormMomentumX()); // = 0
-    myNormMomentumY  = tan(myUserInfo->GetNormMomentumY()); // = 0
-    myNormMomentumZ  = sqrt(1.0 - myNormMomentumX * myNormMomentumX - myNormMomentumY * myNormMomentumY);  // = 1
-
-    E_beam = myUserInfo->GetBeamEnergy() - 0.511*MeV;
-
-    myUserInfo->StoreOriginVertexPositionZ(myEvent->GetVertexZ());
-    myUserInfo->EvtGenStatus = 0;   
-    
-    particleGun->SetParticlePosition(G4ThreeVector(myPositionX,
-						   myPositionY,
-						   myPositionZ ));
-    
-    
-    //Buddhini - Hard code the gun to spit out particles in +-20 deg angles in the horizontal plane.
-//     G4double theta_x = 0.0;
-//     theta_x = CLHEP::RandFlat::shoot(-20,20);
-//     G4cout<<"$$$$ theta_x "<<theta_x<<G4endl;
-//     G4ThreeVector momDir;
-//     momDir.setRThetaPhi(1.,theta_x*deg,0*deg);
-//     particleGun->SetParticleMomentumDirection(momDir);
-
-    particleGun->SetParticleMomentumDirection(G4ThreeVector(myNormMomentumX,
-    							    myNormMomentumY,
-    							    myNormMomentumZ));
-    
-    if (fPolarization == "L") {
-      // longitudinal polarization (after generation)
-      particleGun->SetParticlePolarization((G4ThreeVector(myNormMomentumX,
+  G4int myEventCounter = myUserInfo->GetPrimaryEventNumber();
+  
+  if (myEventCounter%1000==0) G4cout << "*=== Event number = " << myEventCounter << " ===*" << G4endl;
+  
+  G4double myPositionX, myPositionY, myPositionZ, myVertexZ;
+  G4double myNormMomentumX, myNormMomentumY, myNormMomentumZ;
+  G4double E_beam;  // Energy of the incoming and outgoing particle
+  
+  
+  myPositionX =  myUserInfo->GetBeamPositionX();
+  myPositionY =  myUserInfo->GetBeamPositionY();
+  myPositionZ =  myUserInfo->GetBeamPositionZ();
+  
+  myNormMomentumX  = tan(myUserInfo->GetNormMomentumX()); // = 0
+  myNormMomentumY  = tan(myUserInfo->GetNormMomentumY()); // = 0
+  myNormMomentumZ  = sqrt(1.0 - myNormMomentumX * myNormMomentumX - myNormMomentumY * myNormMomentumY);  // = 1
+  
+  E_beam = myUserInfo->GetBeamEnergy() - 0.511*MeV;
+  
+  myUserInfo->StoreOriginVertexPositionZ(myEvent->GetVertexZ());
+  myUserInfo->EvtGenStatus = 0;   
+  
+  particleGun->SetParticlePosition(G4ThreeVector(myPositionX,
+						 myPositionY,
+						 myPositionZ ));
+  
+  particleGun->SetParticleMomentumDirection(G4ThreeVector(myNormMomentumX,
 							  myNormMomentumY,
-							  myNormMomentumZ)));
-    }else if (fPolarization == "V") {
-      // vertical transverse polarization (after generation)
-      particleGun->SetParticlePolarization(G4ThreeVector(myNormMomentumX,
-							 myNormMomentumY,
-							 myNormMomentumZ)
-					   .cross(G4ThreeVector(1,0,0)));
-    }else if (fPolarization == "H") {
-      // horizontal transverse polarization (after generation)
-      particleGun->SetParticlePolarization(G4ThreeVector(myNormMomentumX,
-							 myNormMomentumY,
-							 myNormMomentumZ)
-					   .cross(G4ThreeVector(0,1,0)));
-    }else if(fPolarization == "mV"){
-      particleGun->SetParticlePolarization(G4ThreeVector(myNormMomentumX,
-							 myNormMomentumY,
-							 myNormMomentumZ)
-                                           .cross(G4ThreeVector(-1,0,0)));
-    }else if(fPolarization == "mH"){
-      particleGun->SetParticlePolarization(G4ThreeVector(myNormMomentumX,
-							 myNormMomentumY,
-							 myNormMomentumZ)
-					   .cross(G4ThreeVector(0,-1,0)));
-    }else if(fPolarization == "mL"){
-      particleGun->SetParticlePolarization(G4ThreeVector(-myNormMomentumX,
-							 -myNormMomentumY,
-							 -myNormMomentumZ));
-    }else if(fPolarization == "0"){
-      particleGun->SetParticlePolarization(G4ThreeVector(0,0,0));
-    }else particleGun->SetParticlePolarization(G4ThreeVector(myNormMomentumX,myNormMomentumY,myNormMomentumZ)); //longitudinal by default
-    
-    particleGun->SetParticleEnergy(E_beam);
-    
-    // finally : fire !!!
-    particleGun->GeneratePrimaryVertex(anEvent);  // takes an event, generates primary vertex, and associates primary particles with the vertex
-    myUserInfo->StorePrimaryEventNumber(myEventCounter+1);
-    //myUserInfo->SetBeamEnergy(myEvent->GetBeamEnergy());
-    // rest of userInfo filled in QweakSimSteppingAction.cc
-    
-    //  G4cout << "###### Leaving QweakSimPrimaryGeneratorAction::GeneratePrimaries" << G4endl;
-    
+							  myNormMomentumZ));
+  
+  if (fPolarization == "L") {
+    // longitudinal polarization (after generation)
+    particleGun->SetParticlePolarization((G4ThreeVector(myNormMomentumX,
+							myNormMomentumY,
+							myNormMomentumZ)));
+  }else if (fPolarization == "V") {
+    // vertical transverse polarization (after generation)
+    particleGun->SetParticlePolarization(G4ThreeVector(myNormMomentumX,
+						       myNormMomentumY,
+						       myNormMomentumZ)
+					 .cross(G4ThreeVector(1,0,0)));
+  }else if (fPolarization == "H") {
+    // horizontal transverse polarization (after generation)
+    particleGun->SetParticlePolarization(G4ThreeVector(myNormMomentumX,
+						       myNormMomentumY,
+						       myNormMomentumZ)
+					 .cross(G4ThreeVector(0,1,0)));
+  }else if(fPolarization == "mV"){
+    particleGun->SetParticlePolarization(G4ThreeVector(myNormMomentumX,
+						       myNormMomentumY,
+						       myNormMomentumZ)
+					 .cross(G4ThreeVector(-1,0,0)));
+  }else if(fPolarization == "mH"){
+    particleGun->SetParticlePolarization(G4ThreeVector(myNormMomentumX,
+						       myNormMomentumY,
+						       myNormMomentumZ)
+					 .cross(G4ThreeVector(0,-1,0)));
+  }else if(fPolarization == "mL"){
+    particleGun->SetParticlePolarization(G4ThreeVector(-myNormMomentumX,
+						       -myNormMomentumY,
+						       -myNormMomentumZ));
+  }else if(fPolarization == "0"){
+    particleGun->SetParticlePolarization(G4ThreeVector(0,0,0));
+  }else particleGun->SetParticlePolarization(G4ThreeVector(myNormMomentumX,
+							   myNormMomentumY,
+							   myNormMomentumZ)); //longitudinal by default
+  
+  particleGun->SetParticleEnergy(E_beam);
+  
+  // takes an event, generates primary vertex, and associates primary particles with the vertex
+  particleGun->GeneratePrimaryVertex(anEvent);  
+  myUserInfo->StorePrimaryEventNumber(myEventCounter+1);    
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
