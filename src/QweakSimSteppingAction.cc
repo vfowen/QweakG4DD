c401338c9a9bfc6a7b313c43b5f8f80c0280959e
Ciprian Gal <ciprian@jlab.org>
2015-06-23 15:56:16 -0400

    added depolarization as a function of energy loss in Brem. updated particle type to the PDG value

--- src/QweakSimSteppingAction.cc
+++ src/QweakSimSteppingAction.cc
@@ -22,8 +22,16 @@
 #include "QweakSimCerenkovDetector_PMTSD.hh"
 #include "QweakSimPMTOnly_PMTSD.hh"
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+/*
+  Brem depolarization: PhysRev.114.887
+  implemented only for transverse polarization
+  will decrease the overall polarization by a functional form from figure 5 in the paper      
+*/
+static const int perpNval=128;
+static const double perpXDepol[perpNval]={0.053,0.066,0.075,0.084,0.097,0.112,0.126,0.137,0.148,0.160,0.169,0.180,0.190,0.200,0.210,0.220,0.226,0.236,0.244,0.251,0.258,0.267,0.274,0.283,0.291,0.300,0.308,0.316,0.324,0.332,0.340,0.350,0.357,0.364,0.371,0.379,0.387,0.395,0.403,0.411,0.417,0.425,0.434,0.443,0.453,0.460,0.468,0.477,0.488,0.499,0.509,0.518,0.525,0.532,0.541,0.548,0.555,0.563,0.570,0.576,0.581,0.587,0.594,0.600,0.608,0.616,0.623,0.628,0.635,0.640,0.646,0.653,0.661,0.668,0.676,0.683,0.688,0.694,0.699,0.706,0.709,0.715,0.722,0.728,0.734,0.738,0.745,0.749,0.752,0.757,0.762,0.769,0.776,0.782,0.789,0.795,0.799,0.806,0.810,0.815,0.824,0.830,0.836,0.843,0.847,0.853,0.858,0.864,0.868,0.874,0.881,0.887,0.893,0.900,0.908,0.915,0.920,0.930,0.935,0.945,0.953,0.963,0.969,0.974,0.979,0.985,0.990,0.994};
+static const double perpYDepol[perpNval]={0.203,0.277,0.399,0.522,0.764,0.890,1.250,1.608,1.966,2.208,2.565,2.924,3.281,3.639,4.114,4.472,4.945,5.421,5.895,6.251,6.485,6.960,7.556,8.030,8.504,9.097,9.671,10.145,10.620,11.094,11.804,12.396,12.988,13.579,14.161,14.871,15.580,16.407,17.234,17.709,18.417,19.127,20.072,21.021,21.968,22.794,23.744,24.689,25.871,27.288,28.116,29.179,30.006,30.838,31.901,32.968,34.030,34.975,35.920,36.746,37.455,38.164,38.874,39.935,40.998,41.944,43.007,43.933,44.878,45.822,46.884,47.770,48.951,50.132,51.195,52.376,53.437,54.265,55.096,56.041,56.867,57.694,58.757,59.584,60.529,61.573,62.519,63.227,63.935,64.644,65.472,66.653,67.599,68.662,69.725,70.788,71.615,72.449,73.275,74.107,75.290,76.359,77.304,78.251,78.959,79.906,80.733,81.679,82.506,83.334,84.281,85.109,86.055,87.120,88.068,89.015,89.961,91.028,91.862,92.929,94.000,95.421,96.132,96.725,97.436,98.387,98.862,99.219};
 
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 QweakSimSteppingAction::QweakSimSteppingAction(QweakSimUserInformation* myUInfo, QweakSimEPEvent* myEPEvent)
 {
 
@@ -37,6 +45,8 @@ QweakSimSteppingAction::QweakSimSteppingAction(QweakSimUserInformation* myUInfo,
 
     evtGenStatus = 0;
     targetCenterPositionZ = myUserInfo->TargetCenterPositionZ;
+
+    for(int i=0;i<perpNval;i++) perpDepol.SetPoint(i,perpXDepol[i],perpYDepol[i]);    
 
     fout=new TFile("o_tuple.root","RECREATE");	
     tout=new TNtuple("t","Ntuple primary info in Pb",
@@ -78,10 +88,6 @@ void QweakSimSteppingAction::UserSteppingAction(const G4Step* theStep) {
     
     //scattering only occur inside reaction region of the target, only occur once
     G4ThreeVector thePosition = theTrack->GetPosition();
-    G4double theX = thePosition.getX();
-    G4double theY = thePosition.getY();
-    G4double theZ = thePosition.getZ();
-    
     G4String procName = thePostPoint->GetProcessDefinedStep()->GetProcessName();
     // dEE in MeV -> all Elosses are in MeV
     G4double dEE = thePrePoint->GetKineticEnergy()/MeV-thePostPoint->GetKineticEnergy()/MeV;
@@ -126,17 +132,35 @@ void QweakSimSteppingAction::UserSteppingAction(const G4Step* theStep) {
 
     if(_material->GetName().compare("PBA")==0){ // perform this only in the Radiator
 
-      /*
-	Brem depolarization: PhysRev.114.887
-	implemented only for transverse polarization
-	will decrease the overall polarization by a functional form from figure 5 in the paper      
-      */
-      // if(_polarization.getR()>0.2 && particleType==G4Electron::ElectronDefinition() ){
-      //   G4cout<<G4endl<<_polarization.getR()<<" "<<_polarization.getX()<<" "<<_polarization.getY()<<" "<<_polarization.getZ()<<" "<<G4endl;
-      //   theStep->GetTrack()->SetPolarization(_polarization*0.9);
-      //   G4cout<<_polarization.getR()<<" "<<_polarization.getX()<<" "<<_polarization.getY()<<" "<<_polarization.getZ()<<" "<<G4endl;
-      // }    
+      if(_pn.compare("eBrem")==0 &&                           // only for eBrem
+	 (_polarization.getX()>0 || _polarization.getY()>0)){ // only for transverse polarization
+	/*
+	  Brem depolarization: PhysRev.114.887
+	  implemented only for transverse polarization
+	  will decrease the overall polarization by a functional form from figure 5 in the paper      
+	*/
+	G4double eLossPercent = 1. - thePostPoint->GetKineticEnergy()/thePrePoint->GetKineticEnergy();
+	G4double depol=0;
+	if( eLossPercent > perpXDepol[perpNval-1]) depol = 1;
+	else if( eLossPercent >= perpXDepol[0] ) depol = perpDepol.Eval(eLossPercent,0,"S")/100.;
+	else depol = 0;
 
+	if(debugPrint){
+	  G4cout<<" ~~~ eBrem depol : "<<eLossPercent<<"% E loss means "<< depol <<" depolarization"<<G4endl;
+	  G4cout<<" ~~~ kin E pre post "<<thePrePoint->GetKineticEnergy()<<" "<<thePostPoint->GetKineticEnergy()<<G4endl;
+	  G4cout<<" ~~~~ polarization X Y Z "
+		<<_polarization.getX()<<" "
+		<<_polarization.getY()<<" "
+		<<_polarization.getZ()<<" => ";
+	}
+	theStep->GetTrack()->SetPolarization(_polarization * (1-depol));
+	if(debugPrint){
+	  G4cout<<_polarization.getX()<<" "
+		<<_polarization.getY()<<" "
+		<<_polarization.getZ()<<G4endl;
+	}
+      }
+      
       // either write out only the primaries or all e\pm and gammas
       if(((particleType==G4Positron::PositronDefinition() ||
 	   particleType==G4Electron::ElectronDefinition() ||
@@ -191,13 +215,7 @@ void QweakSimSteppingAction::UserSteppingAction(const G4Step* theStep) {
 	_var[23]=myUserInfo->GetPrimaryEventNumber();
 	_var[24]=_trackID;
 	_var[25]=_parentID;
-	
-	G4cout<<" PDGEncoding for electron "<<particleType->GetPDGEncoding()<<G4endl;
-	if(particleType==G4Positron::PositronDefinition()) _var[26]=-11;
-	else if(particleType==G4Gamma::GammaDefinition()) _var[26]=22;
-	else if(particleType==G4Electron::ElectronDefinition()) _var[26]=11;
-	else _var[26]=-999;
-	
+	_var[26]=particleType->GetPDGEncoding();	
 	_var[27]=_polarization.getX();
 	_var[28]=_polarization.getY();
 	_var[29]=_polarization.getZ();
