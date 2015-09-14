0506038d3c04c27343569f506377e5c6efa4c4c4
Ciprian Gal <ciprian@jlab.org>
2015-09-14 18:20:11 -0400

    resolve merge conflict

--- scripts/submitJLabJob.py
+++ scripts/submitJLabJob.py
@@ -53,8 +53,8 @@ def createXMLfile(idname,directory,email,source):
 
 def main():
     
-    _xPos=[0]
-#    _xPos=[-10,-5,0,5,10]
+#    _xPos=[0]
+    _xPos=[-5,-3,-2,-1,1,2,3,5]
     _email="ciprian@jlab.org"
     _source="/w/hallc-scifs2/qweak/ciprian/simCodeG410/QweakG4DD"
     _directory="/lustre/expphy/volatile/hallc/qweak/ciprian/farmoutput/g41001p01/twoPh/1e2"
@@ -76,7 +76,7 @@ def main():
         call(["cp",_source+"/myQweakCerenkovOnly.mac",_directory+"/jobs/"+_idN+"/myQweakCerenkovOnly.mac"])
 
 	if submit==1:
-	  print "submitting X position", xP," for the ",nr,"th time"
+	  print "submitting X position", xP," pol ",_pol," for the ",nr,"th time"
 	  call(["jsub","-xml",_directory+"/jobs/"+_idN+"/job.xml"])
 	else:
 	  print "do not submit ",submit
