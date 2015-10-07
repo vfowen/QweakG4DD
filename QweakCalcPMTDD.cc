3ea6b79585819c7635d6104eaff1288e4037804a
Ciprian Gal <ciprian@jlab.org>
2015-10-07 17:31:39 -0400

    first commit of calculation of pmtdd with 3d interpolation

---
+++ QweakCalcPMTDD.cc
@@ -0,0 +1,246 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <math.h>
+
+#include "TH3D.h"
+#include "TFile.h"
+#include "TTree.h"
+
+using namespace std;
+
+
+const int dimension=5;//3 DoF + 2 PE values
+vector<double> scanPoints[dimension];
+const int debugPrint=0;
+
+void readPEs();
+void getCorners(int lowerIndex, int upperIndex, int depth, std::vector<double> point,
+		std::vector<double> points[dimension]);
+void getPEs(std::vector<double> in[dimension], std::vector<double> pt,
+	    double &outL, double &outR);
+
+int main(int argc, char** argv)
+{
+
+  if( argc !=2 ) {
+    cout<<" usage: build/QweakCalcPMTDD [path to infile with distributions from QweakSimG4 trees]"<<endl;
+    return 1;
+  }
+
+  readPEs();
+
+  string file(argv[1]);
+  TFile *fin=TFile::Open(file.c_str(),"READ");
+
+  string part[2]={"Pe","Ae"};
+  string partTit[2]={"Primary e-","All e"};
+
+  for(int i=0;i<2;i++){
+    string histNm="dist_"+part[i];
+    TH3D *dist=(TH3D*)fin->Get(histNm.c_str());
+
+    double lTotPE(0),rTotPE(0);
+    std::vector<double> pt(dimension-2,0);
+    std::vector<double> pts[dimension];
+    
+    for(int xx=1;xx<=dist->GetXaxis()->GetNbins();xx++)//position
+      for(int yy=1;yy<=dist->GetYaxis()->GetNbins();yy++)//angle
+	for(int zz=1;zz<=dist->GetZaxis()->GetNbins();zz++){//energy
+	  pt[0]=dist->GetXaxis()->GetBinCenter(xx);
+	  pt[1]=dist->GetYaxis()->GetBinCenter(yy);
+	  pt[2]=dist->GetZaxis()->GetBinCenter(zz);
+
+	  if(fabs(pt[0])>=40) continue;
+	  if(fabs(pt[1])>=57) continue;
+	  if(fabs(pt[2])>=100) continue;
+	  
+	  getCorners(0,scanPoints[0].size(),0,pt,pts);
+	  double rpe(-1),lpe(-1);
+	  getPEs(pts,pt,lpe,rpe);
+	  if(lpe!=-1 && rpe!=-1){
+	    lTotPE+=lpe;
+	    rTotPE+=rpe;
+	    //cout<<"interpolated numbers "<<lpe<<" "<<rpe<<endl;
+	  }else{
+	    cout<<"Problem with interpolator!"<<lpe<<" "<<rpe<<" "<<pt[0]<<" "<<pt[1]<<" "<<pt[2]<<endl;
+	    exit(1);
+	  }
+	      	  
+	}
+    cout<<partTit[i]<<" : L R (L-R)/(L+R)"<<lTotPE<<" "<<rTotPE<<" "<<(lTotPE-rTotPE)/(lTotPE+rTotPE)<<endl;
+  }
+  
+  fin->Close();
+  
+
+  return 0;
+  
+}
+
+void readPEs(){
+  ifstream fin("output/buddhiniAngle/angle_scans_fixed/md3_angle_scan.txt");
+  double x1,x2,x3,x4,x5,x6,x7,x8,x9;  
+  string data;
+  getline(fin,data);
+
+  for(int i=0;i<dimension;i++)
+    scanPoints[i].clear();
+  
+  while(fin>>x1>>x2>>x3>>x4>>x5>>x6>>x7>>x8>>x9){
+    scanPoints[0].push_back(x1);//position
+    scanPoints[1].push_back(x2);//energy
+    double ang=asin(tan(x3*3.14159265359/180.))/3.14159265359*180.;
+    scanPoints[2].push_back(ang);//angle
+    scanPoints[3].push_back(x6);//LPEs
+    scanPoints[4].push_back(x8);//RPEs
+    //cout<<x1<<" "<<x2<<" "<<ang<<" "<<x6<<" "<<x8<<endl;
+  }
+  
+  fin.close();
+}
+
+void getCorners(int lowerIndex, int upperIndex, int depth, std::vector<double> point,
+		std::vector<double> points[dimension]){
+
+  if(lowerIndex==-1 || upperIndex==-1 || lowerIndex>upperIndex){
+    cout<<"Problem with index: "<<lowerIndex<<" "<<upperIndex<<endl;
+    exit(0);
+  }
+  if(lowerIndex==upperIndex) return;
+  
+  int lI(-1),hI(-1);
+  double valSmaller(999),valLarger(999);
+  int nextDepth=depth+1;
+  
+  std::vector<double>::iterator begin=scanPoints[depth].begin();
+  std::vector<double>::iterator start=begin+lowerIndex;
+  std::vector<double>::iterator stop =begin+upperIndex;
+
+  if(debugPrint)
+    cout<<"start upper : "<<point[depth]<<" "<<*start<<" "<<*(stop-1)<<" "
+	<<int(start-begin)<<" "<<int(stop-begin)<<endl;
+
+  if( point[depth]== *start)
+    lI=lowerIndex;
+  else if( point[depth] == *(stop-1) ){
+    lI = int( lower_bound(start,stop,point[depth]) - begin );
+  }else{    
+    valSmaller = *( lower_bound(start,stop,point[depth]) - 1 );
+    lI = int( lower_bound(start,stop,valSmaller) - begin );
+  }
+  
+  hI = int( upper_bound(start,stop,point[depth]) - begin );
+
+  if(debugPrint){
+    cout<<depth<<" "<<lowerIndex<<" "<<upperIndex<<" "<<lI<<" "<<hI<<endl;
+    cout<<" "<<valSmaller<<" "<<point[depth]<<" "<<valLarger<<" "
+	<<scanPoints[depth][lI]<<" "<<scanPoints[depth][hI-1]<<endl;
+  }
+
+  if(depth==dimension-3){
+
+    for(int i=0;i<dimension;i++) {
+      points[i].push_back(scanPoints[i][lI]);
+      points[i].push_back(scanPoints[i][hI]);
+    }
+    
+    if(debugPrint){
+      cout<<endl<<endl<<"End lower: "<<endl;
+      for(int i=0;i<dimension;i++) cout<<scanPoints[i][lI]<<" ";
+      cout<<endl;
+      for(int i=0;i<dimension;i++) cout<<scanPoints[i][hI]<<" ";
+      cout<<endl<<endl;
+    }
+    return;
+  }else{
+    getCorners(lI,hI,nextDepth,point,points);
+  }
+
+  if( point[depth] == *(stop-1) ) return;
+
+  if(debugPrint)
+    cout<<"start upper : "<<depth<<" "<<point[depth]<<" "<<*start<<" "<<*(stop-1)<<" "
+	<<int(start-begin)<<" "<<int(stop-begin)<<endl;
+  
+  lI = int( upper_bound(start,stop,point[depth]) - begin );
+  if( point[depth] == *(stop-1) )
+    hI = upperIndex;
+  else{
+    valLarger=*(lower_bound(start,stop,point[depth]));
+    hI = int( upper_bound(start,stop,valLarger) - begin );
+  }
+  if(debugPrint){
+    cout<<depth<<" "<<lowerIndex<<" "<<upperIndex<<" "<<lI<<" "<<hI<<endl;
+    cout<<" "<<valSmaller<<" "<<point[depth]<<" "<<valLarger<<" "
+	<<scanPoints[depth][lI]<<" "<<scanPoints[depth][hI-1]<<endl;
+  }
+  
+  if( point[depth]!= *start )
+    getCorners(lI,hI,nextDepth,point,points);
+}
+
+void getPEs(std::vector<double> in[dimension], std::vector<double> pt,
+	    double &outL, double &outR){
+
+  std::vector<double> dm[dimension];
+
+  if(debugPrint){
+    cout<<"start "<<in[0].size()<<endl;
+    for(unsigned long i=0;i<in[0].size();i++){
+      for(int j=0;j<dimension;j++)
+	cout<<in[j][i]<<" ";
+      cout<<endl;
+    }
+  }
+
+  
+  if(in[0].size()==1){
+    outL=in[dimension-2].front();
+    outR=in[dimension-1].front();
+    return;
+  }
+
+  int depth(-1);
+  for(int j=0;j<dimension-2;j++)
+    if(in[j][0]!=in[j][1])
+      depth=j;
+  
+  for(int i=0;i<dimension;i++){
+    dm[i].resize(in[i].size());
+    for(unsigned long j=0;j<in[i].size();j++)
+      dm[i][j]=in[i][j];
+  }
+
+  for(int i=0;i<dimension;i++)
+    in[i].resize(dm[i].size()/2);
+
+  for(unsigned long i=0;i<dm[0].size();i+=2){
+
+    double l1=dm[dimension-2][i];
+    double r1=dm[dimension-1][i];
+    double l2=dm[dimension-2][i+1];
+    double r2=dm[dimension-1][i+1];
+    double fl=l1+(l2-l1)*(pt[depth]-dm[depth][i])/(dm[depth][i+1]-dm[depth][i]);
+    double fr=r1+(r2-r1)*(pt[depth]-dm[depth][i])/(dm[depth][i+1]-dm[depth][i]);
+      
+    for(int j=0;j<dimension-2;j++)
+      if(j!=depth)
+	in[j][i/2]=dm[j][i];
+      else
+	in[j][i/2]=pt[depth];
+    in[dimension-2][i/2]=fl;
+    in[dimension-1][i/2]=fr;      
+  }
+
+  if(debugPrint){
+    cout<<endl<<depth<<endl;
+    for(unsigned long i=0;i<in[0].size();i++){
+      for(int j=0;j<dimension;j++)
+	cout<<in[j][i]<<" ";
+      cout<<endl;
+    }
+  }
+  getPEs(in,pt,outL,outR);
+}
