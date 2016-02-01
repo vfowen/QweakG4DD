f4f0fc06150c2b6492d8e1f29a86c18cb989298c
Ciprian Gal <ciprian@jlab.org>
2016-02-01 15:47:37 -0500

    update macro to analyze one file or list of files. no more asym hist output

--- QweakGetPEs.cc
+++ QweakGetPEs.cc
@@ -12,18 +12,16 @@
 #include "TH1D.h"
 
 using namespace std;
+void processOne(TTree *QweakSimG4_Tree,double &LnPMThit,double &RnPMThit, int ndet);
 
 int main(int argc, char** argv)
 {
   if(argc != 3) {
-    cout<<" run as: build/QweakGetPEs [detector number] [path to infile with list of output QweakSimG4 trees] "<<endl;
+    cout<<" run as: build/QweakGetPEs [detector number] [path to QweakSimG4 output file or to infile with list of output QweakSimG4 trees] "<<endl;
     return 1;
   }
   int ndet=atoi(argv[1]);
   string files(argv[2]);
-
-  ifstream ifile(files.c_str());
-  char _data[500];
 
   double lTotPE(0),rTotPE(0);
   double LnPMThit(0),RnPMThit(0),asymPMT(0);
@@ -32,50 +30,62 @@ int main(int argc, char** argv)
   
   TH1D *lpe=new TH1D("lpe","left  nb of PEs per event",500,0,500); 
   TH1D *rpe=new TH1D("rpe","right nb of PEs per event",500,0,500); 
-  TH1D *asymPE=new TH1D("asymPE","PE asym",201,-1.,1.01);
-  
-  while(ifile>>_data){
-    TFile *fin=TFile::Open(_data);
-    TTree *QweakSimG4_Tree=(TTree*)fin->Get("QweakSimG4_Tree");
-    cout<<"processing : "<<_data<<" for detector "<<ndet<<endl;
-        
-    //set addresses of leafs
-    QweakSimUserMainEvent* event = 0;
-    QweakSimG4_Tree->SetBranchAddress("QweakSimUserMainEvent",&event);
-    
-    for (int i = 0; i < QweakSimG4_Tree->GetEntries(); i++) {
-      QweakSimG4_Tree->GetEntry(i);
-      if(i%10000==1) cout<<" at event: "<<i<<endl;
-      //loop over hits
-      LnPMThit=-2;
-      RnPMThit=-2;
-      asymPMT=-2;
-      
-      if(event->Cerenkov.PMT.GetDetectorNbOfHits()>0){
-	LnPMThit=event->Cerenkov.PMT.GetPMTLeftNbOfHits()[ndet];//is the same as NbOfPEs
-	RnPMThit=event->Cerenkov.PMT.GetPMTRightNbOfHits()[ndet];
-	lpe->Fill(LnPMThit);
-	rpe->Fill(RnPMThit);
-	lTotPE+=LnPMThit;
-	rTotPE+=RnPMThit;      
-      }      
-      
-      if( LnPMThit!=-2 && RnPMThit!=-2 && (LnPMThit+RnPMThit)>0 ){
-	asymPMT=(LnPMThit-RnPMThit)/(LnPMThit+RnPMThit);
-	asymPE->Fill(asymPMT);
-      }
+
+  if ( files.find(".root") < files.size() ){
+    TFile *fin=new TFile(files.c_str(),"READ");
+    if(!fin->IsOpen()){
+      cout<<"Problem: can't find file: "<<files<<endl;
+      fout->Close();
+      return -3;
     }
     
+    TTree *QweakSimG4_Tree=(TTree*)fin->Get("QweakSimG4_Tree");
+    cout<<"processing only one file: "<<files.c_str()<<" for detector "<<ndet<<endl;        
+    cout<<" total nr ev: "<<QweakSimG4_Tree->GetEntries()<<endl;
+    
+    processOne(QweakSimG4_Tree,LnPMThit,RnPMThit,ndet);
+    lTotPE+=LnPMThit;
+    rTotPE+=RnPMThit;      
+    fin->Close();
+    delete fin;
+  }else{  
+    ifstream ifile(files.c_str());
+    char _data[500];
+    while(ifile>>_data){
+      TFile *fin=TFile::Open(_data);
+      TTree *QweakSimG4_Tree=(TTree*)fin->Get("QweakSimG4_Tree");
+      cout<<"processing : "<<_data<<" for detector "<<ndet<<endl;
+
+      processOne(QweakSimG4_Tree,LnPMThit,RnPMThit,ndet);
+      lTotPE+=LnPMThit;
+      rTotPE+=RnPMThit;          
+    }
     cout<<"Done loop writting out!"<<endl;
+    ifile.close();
   }
+
 
   fout->cd();
   lpe->Write();
   rpe->Write();
-  asymPE->Write();
   fout->Close();
   cout<<"Total number of PEs: "<<setprecision(12)<<lTotPE<<" "<<rTotPE<<endl;
   
-  ifile.close();
   return 0;
+}
+
+
+void processOne(TTree *QweakSimG4_Tree,double &LnPMThit,double &RnPMThit, int ndet){
+  QweakSimUserMainEvent* event = 0;
+  QweakSimG4_Tree->SetBranchAddress("QweakSimUserMainEvent",&event);
+      
+  for (int i = 0; i < QweakSimG4_Tree->GetEntries(); i++) {
+    QweakSimG4_Tree->GetEntry(i);
+    if(i%10000==1) cout<<" at event: "<<i<<endl;
+    
+    if(event->Cerenkov.PMT.GetDetectorNbOfHits()>0){
+      LnPMThit=event->Cerenkov.PMT.GetPMTLeftNbOfHits()[ndet];//is the same as NbOfPEs
+      RnPMThit=event->Cerenkov.PMT.GetPMTRightNbOfHits()[ndet];
+    }      
+  }
 }
