cfd88601b72aac18c4d8f5283f3880d016934ae2
Ciprian Gal <ciprian@jlab.org>
2016-03-01 15:07:14 -0500

    rewrite and improve general submission script

--- scripts/submitJLabJob.py
+++ scripts/submitJLabJob.py
@@ -14,38 +14,37 @@ def main():
     _tracking=2# 2=no optical ph and 10x faster than 3=full
     _email="ciprian@jlab.org"
     _source="/lustre/expphy/work/hallc/qweak/ciprian/simCodeG410/QweakG4DD"
-#    _source="/w/hallc-scifs2/qweak/ciprian/simCodeG410/QweakG4DD"
-    _directory="/lustre/expphy/volatile/hallc/qweak/ciprian/farmoutput/g41001p01/mott/updateCH/mottX1e2/001mStep"
-    _nEv=30000
-    _nrStop=10000
-    _nrStart=7000
+    _directory="/lustre/expphy/volatile/hallc/qweak/ciprian/farmoutput/g41001p01/mott/updateCH/mottX1e2/005mStep"
+    _nEv=50000
+    _nrStop=1000
+    _nrStart=500
     _pol="V"
     submit=1
-    #_nrJobs=[] 
-    #for nr in _nrJobs:#this is for specific job resubmission   
+
+    idRoot= _pol+'x_%04d_%06.2f_%06.2f_%06.2f_%06.2f_%06.2f'% (_beamE,_xP,_yP,_zP,_Px,_Py) 
     for nr in range(_nrStart,_nrStop): # repeat for nr jobs
-#        time.sleep(2)
-        _idN= _pol+'_%04d_%06.2f_%06.2f_%06.2f_%06.2f_%06.2f_%04d'% (_beamE,_xP,_yP,_zP,_Px,_Py,nr) 
+        _idN= idRoot+'_%04d'%(nr) 
         createMacFile(_directory,_idN,_xP,_yP,_zP,_Px,_Py,_tracking,_beamE,_pol,_nEv,nr)
-        createXMLfile(_idN,_directory,_email,_source)
         call(["cp",_source+"/build/QweakSimG4",_directory+"/jobs/"+_idN+"/QweakSimG4"])
         call(["cp",_source+"/build/QweakAna",_directory+"/jobs/"+_idN+"/QweakAna"])
         call(["cp",_source+"/myQweakCerenkovOnly.mac",_directory+"/jobs/"+_idN+"/myQweakCerenkovOnly.mac"])
 
-	if submit==1:
-	  print "submitting position (", _xP,_yP,_zP,")"
-          print " and momDirection (",_Px,_Py,") with pol",_pol," for the ",nr,"th time"
-	  call(["jsub","-xml",_directory+"/jobs/"+_idN+"/job.xml"])
-	else:
-	  print "do not submit ",submit
+    createXMLfile(_source,_directory,idRoot,_nrStart,_nrStop,_email)
+    if submit==1:
+        print "submitting position (", _xP,_yP,_zP,")"
+        print " and momDirection (",_Px,_Py,") with pol",_pol," between ",_nrStart,_nrStop
+        call(["jsub","-xml",_source+"/scripts/jobs/"+idRoot+".xml"])
+    else:
+        print "do not submit ",submit
+
     print "I am all done"
 
 def createMacFile(directory,idname,
                   xPos,yPos,zPos,
                   Px,Py,tracking,
                   beamE,pol,nEv,nr):
-    if not os.path.exists(directory+"/jobs/"+idname):
-        os.makedirs(directory+"/jobs/"+idname)
+    if not os.path.exists(directory+"/jobs/"+idname+"/log"):
+        os.makedirs(directory+"/jobs/"+idname+"/log")
    
     f=open(directory+"/jobs/"+idname+"/myRun.mac",'w')
     f.write("/control/execute myQweakCerenkovOnly.mac\n")
@@ -66,28 +65,39 @@ def createMacFile(directory,idname,
     f.close()
     return 0
 
-def createXMLfile(idname,directory,email,source):
-    if not os.path.exists(directory+"/jobs/"+idname+"/log"):
-        os.makedirs(directory+"/jobs/"+idname+"/log")
+def createXMLfile(source,writeDir,idRoot,nStart,nStop,email):
     
-    f=open(directory+"/jobs/"+idname+"/job.xml","w")
+    if not os.path.exists(source+"/scripts/jobs"):
+        os.makedirs(source+"/scripts/jobs")
+
+    f=open(source+"/scripts/jobs/"+idRoot+".xml","w")
     f.write("<Request>\n")
     f.write("  <Email email=\""+email+"\" request=\"false\" job=\"true\"/>\n")
     f.write("  <Project name=\"qweak\"/>\n")
+#    f.write("  <Track name=\"debug\"/>\n")
     f.write("  <Track name=\"simulation\"/>\n")
-    f.write("  <Name name=\""+idname+"\"/>\n")
+    f.write("  <Name name=\""+idRoot+"\"/>\n")
     f.write("  <OS name=\"centos65\"/>\n")
     f.write("  <Command><![CDATA[\n")
-    f.write("cd "+directory+"/jobs/"+idname+"\n")
-    f.write("./QweakSimG4 myRun.mac\n")
-    f.write("./QweakAna 3 QwSim_0.root\n")
+    f.write("QweakSimG4 myRun.mac\n")
+    f.write("QweakAna 3 QwSim_0.root\n")
     f.write("  ]]></Command>\n")
     f.write("  <Memory space=\"2000\" unit=\"MB\"/>\n")
-#    f.write("  <TimeLimit unit=\"minutes\" time=\"1200\"/>\n")
-    f.write("  <Job>\n")
-    f.write("    <Stdout dest=\""+directory+"/jobs/"+idname+"/log/log.out\"/>\n")
-    f.write("    <Stderr dest=\""+directory+"/jobs/"+idname+"/log/log.err\"/>\n")
-    f.write("  </Job>\n")
+
+    for nr in range(nStart,nStop): # repeat for nr jobs
+        f.write("  <Job>\n")
+        idName= writeDir+"/jobs/"+idRoot+'_%04d'%(nr)
+        f.write("    <Input src=\""+idName+"/QweakSimG4\" dest=\"QweakSimG4\"/>\n")
+        f.write("    <Input src=\""+idName+"/QweakAna\" dest=\"QweakAna\"/>\n")
+        f.write("    <Input src=\""+idName+"/myQweakCerenkovOnly.mac\" dest=\"myQweakCerenkovOnly.mac\"/>\n")
+        f.write("    <Input src=\""+idName+"/myRun.mac\" dest=\"myRun.mac\"/>\n")
+        f.write("    <Output src=\"QwSim_0.root\" dest=\""+idName+"/QwSim_0.root\"/>\n")
+        f.write("    <Output src=\"o_tuple.root\" dest=\""+idName+"/o_tuple.root\"/>\n")
+        f.write("    <Output src=\"o_dist.root\" dest=\""+idName+"/o_dist.root\"/>\n")
+        f.write("    <Stdout dest=\""+idName+"/log/log.out\"/>\n")
+        f.write("    <Stderr dest=\""+idName+"/log/log.err\"/>\n")
+        f.write("  </Job>\n\n")
+
     f.write("</Request>\n")
     f.close()
     return 0
