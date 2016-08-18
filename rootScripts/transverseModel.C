246fe55fba9d7137ccb8c1c82276ccec79016bb3
Ciprian Gal <ciprian@jlab.org>
2016-08-18 17:49:38 -0400

    small updates to macro

--- rootScripts/transverseModel.C
+++ rootScripts/transverseModel.C
@@ -1,8 +1,8 @@
 void transverseModel(){
-  cout<<cdf(2.0,90.0)<<endl;
+  cout<<cdf(1.0,90.0)<<endl;
 
-  c2=new TCanvas();
-  TF2 *f2=new TF2("f2","1/x*sin(y)",1,1000,0,3.14159265/2);
+  // c2=new TCanvas();
+  // TF2 *f2=new TF2("f2","1/x*sin(y)",1,1000,0,3.14159265/2);
   // f2->SetNpx(10000);
   // f2->SetNpy(1000);
   //f2->Draw("colz");
@@ -10,8 +10,9 @@ void transverseModel(){
 
 double cdf(double e=1, double th=90)
 {
-  double pi=3.14159265;
+  double pi=acos(-1);
   TH1F *rf=new TH1F("rf","randf",300,0,2*pi);
+  //TH1F *rf=new TH1F("rf","randf",300,-2*pi,4*pi);
   TH1F *rf1=new TH1F("rf1","randf",300,0,2*pi);
   TF1 *fit=new TF1("fit","[0]+[1]*sin(x+[2])",0,2*pi);
 
@@ -20,15 +21,19 @@ double cdf(double e=1, double th=90)
   double pre=1.0/e*sin(th/180.0*pi);
   cout<<pre<<endl;
   for(int i=0;i<100000;i++){
-    double y=a.Uniform()*2*pi;
     double pr=c.Uniform();
 
-    if(pr < sin(y-pi)*pre )
+    double y=a.Uniform()*2*pi;    
+    
+    //if(pr < sin(y-pi)*pre )
+    if(pr < sin(y) )
       y-=pi;
 
+    if(y<0) y+=2*pi;
+    else if(y>2*pi) y=fmod(y,2*pi);
+
     rf->Fill(y);
-    //  if(pr < sin(y-pr) ) rf->Fill(y-pi);
-    // else rf->Fill(y);
+
   }
 
   gStyle->SetOptStat(0);
