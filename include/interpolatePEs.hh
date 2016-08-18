8848790338d58bab0514ac70fed12c534dfecad0
Ciprian Gal <ciprian@jlab.org>
2016-08-18 17:51:12 -0400

    move interpolation to a class so that I can easily make changes and updates in all macros

---
+++ include/interpolatePEs.hh
@@ -0,0 +1,38 @@
+#ifndef interpolatePEs_hh
+#define interpolatePEs_hh 1
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <math.h>
+#include <algorithm>
+
+class interpolatePEs
+{
+private:
+  double xPosLowLimit, xPosHighLimit;
+  double xAngLowLimit, xAngHighLimit;
+  double energyLowLimit, energyHighLimit;
+
+  static const int dimension=5;//3 DoF + 2 PE values
+  std::vector<double> scanPoints[dimension];
+  std::string barModel;
+  
+  void getCorners(int lowerIndex, int upperIndex, int depth, std::vector<double> point,
+		  std::vector<double> points[dimension]);
+
+  void getPEs(std::vector<double> in[dimension], std::vector<double> pt,
+	      double &outL, double &outR);
+
+public:
+  interpolatePEs(std::string bar);
+  ~interpolatePEs(){};
+  
+  int verbosity;
+  int getPEs(double E, double x, double angX ,double &outL,double &outR);
+  void readScan();
+
+};
+
+#endif
